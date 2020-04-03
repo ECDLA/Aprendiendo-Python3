8a0014e109fcb0da493abe627aac5ada6ead2fce
JCVBS <jcvb2004@gmail.com>
2020-04-02 19:59:28 -0500

    Cambie el nombre del archivo system.py a utils.py, y movi dicho archivo al proyecto.

---
+++ utils.py
@@ -0,0 +1,16 @@
+# Author: JCVBS / 67
+
+import sys
+import os
+
+OPERATING_SYSTEM_TYPE = sys.platform
+
+def clear():
+	if OPERATING_SYSTEM_TYPE in ['linux', 'linux2']:
+		os.system('clear')
+
+	elif OPERATING_SYSTEM_TYPE in ['win32', 'cygwin', 'msys']:
+		os.system('cls')
+
+	else:
+		print("Lo lamento, tu terminal no es compatible :(")
